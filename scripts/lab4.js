35a8363ebf42edf14c8dbf3e877dc53aa1cdec2f
Alan De Luna <alandeluna18@gmail.com>
2025-04-24 19:21:39 -0700

    Fixed Possible Bugs

--- scripts/lab4.js
+++ scripts/lab4.js
@@ -5,15 +5,18 @@
  * @param {*} add, boolean value to tell the function what to do. 
  * @returns The sum of the two numbers if add is true and false otherwise.
  */
-function sumValues(num1, num2, add) {
-    if (add) {
-        const result = 0;
+function sumValues(num1, num2, add) 
+{
+    if (add) 
+    {
+        let result = 0;
 
         result = num1 + num2;
 
         return result;
     }
-    else {
+    else 
+    {
         return !add;
     }
 }
@@ -24,9 +27,10 @@ function sumValues(num1, num2, add) {
  * @param {*} discount, a number between 0-1 to represent the discount. 
  * @returns An array of each price's new price, after the discount is applied. Or false, if prices array is empty.
  */
-function discountPrices(prices, discount) {
-    const discounted = []
-    const length = prices.length;
+function discountPrices(prices, discount) 
+{
+    let discounted = []
+    let length = prices.length;
     let discountedPrice = 0
     for(let i = 0; i < length; i++) {
         discountedPrice += prices[i] * (1 - discount);
